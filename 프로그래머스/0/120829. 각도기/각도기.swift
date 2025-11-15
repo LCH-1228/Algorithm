56f799b28dae1c4930e3d1d601d6bd45a4ab56ab
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2025-11-15 20:52:31 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    guard angle >= 0 && angle <= 180 else { return 0 }
+    switch angle {
+    case 0..<90:
+        return 1
+    case 90:
+        return 2
+    case 91..<180:
+        return 3
+    case 180:
+        return 4
+    default:
+        return 0
+    }
+}
