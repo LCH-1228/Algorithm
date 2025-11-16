de03c3da009592f7634afe438b8ef9ac51d97d80
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2025-11-16 19:15:35 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    guard num1 >= 0 && num1 <= 100 else { return 0 }
+    guard num2 >= 0 && num2 <= 100 else { return 0 }
+    return num1 / num2
+}
