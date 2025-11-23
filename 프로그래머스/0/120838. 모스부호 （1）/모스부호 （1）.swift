1f852bca8bb35cd582bc56bcdb3583293a692fb5
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2025-11-23 11:21:04 +0900

    [level 0] Title: 모스부호 (1), Time: 0.20 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ letter:String) -> String {
+  let morse: [String: String] = [
+      ".-":"a","-...":"b","-.-.":"c","-..":"d",".":"e","..-.":"f",
+      "--.":"g","....":"h","..":"i",".---":"j","-.-":"k",".-..":"l",
+      "--":"m","-.":"n","---":"o",".--.":"p","--.-":"q",".-.":"r",
+       "...":"s","-":"t","..-":"u","...-":"v",".--":"w","-..-":"x",
+      "-.--":"y","--..":"z"
+    ]
+  
+  return letter.components(separatedBy: " ")
+    .compactMap{ morse[$0] }
+    .joined()
+}
