1c8f0a6903130fc44c51fb8877be6d86f41f2fdf
LCH-1228 <chanho1228@gmail.com>
2025-12-02 21:43:14 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
@@ -1,6 +1,18 @@
 func solution(_ a:Int, _ b:Int) -> Int64 {
+    /*
+    기존 풀이 - O(n)
     guard a != b else { return Int64(a) }
     let min = a < b ? a : b
     let max = a < b ? b : a
     return Int64((min...max).reduce(0, +))
+    */
+    
+    /*
+    등차수열의 합 - O(1)
+    S = (첫항 + 끝항) * 항의 개수 / 2
+    */
+    guard a != b else { return Int64(a) }
+    let min = a < b ? a : b
+    let max = a < b ? b : a
+    return Int64(min + max) * Int64(max - min + 1) / Int64(2)
 }
