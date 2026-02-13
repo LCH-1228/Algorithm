194989e32f58732757d86b1bdd569dce95e94b5f
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-13 19:36:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.45 ms, Memory: 25.2 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
@@ -1,5 +1,14 @@
 func solution(_ arr:[Int]) -> [Int] {
     guard arr.count != 1 else { return [-1] }
-    let target = arr.min()!
-    return arr.filter { $0 != target }
+    var result = arr
+    var minIndex = 0
+    
+    for i in 0..<arr.count {
+       if arr[i] < arr[minIndex] {
+          minIndex = i
+        }
+    }
+
+    result.remove(at: minIndex)
+    return result
 }
