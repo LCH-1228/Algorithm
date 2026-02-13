a1929506be92ad8bd1578649613cb3d761f134a1
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-13 19:37:42 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.37 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
@@ -1,5 +1,13 @@
 func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    return zip(arr1, arr2).map { row1, row2 in
-        zip(row1, row2).map(+)
+    var result = [[Int]]()
+    
+    for i in 0..<arr1.count {
+        var temp = [Int]()
+        for j in 0..<arr1[i].count {
+            temp.append(arr1[i][j] + arr2[i][j])
+        }
+        result.append(temp)
     }
+    
+    return result
 }
