b9f1bed081a274990d4585aea969fd88ec414694
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-14 20:29:26 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
@@ -1,7 +1,13 @@
 import Foundation
 
 func solution(_ myString:String, _ letter:String) -> String {
-    guard myString.count >= 1 && myString.count <= 100 else { return "" }
-    guard letter.count == 1 else { return "" }
-    return myString.filter{ String($0) != letter }
+    var result = [String]()
+    
+    for char in myString {
+        if char != Character(letter) {
+            result.append(String(char))
+        }
+    }
+    
+    return result.joined()
 }
