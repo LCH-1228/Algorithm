3586e87d82e01cbe289c658d968aa459803f4ef3
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-14 21:53:24 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.swift
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.swift
@@ -1,8 +1,11 @@
 import Foundation
 
 func solution(_ array:[Int], _ height:Int) -> Int {
-    guard array.count >= 1 && array.count <= 100 else { return 0 }
-    guard height >= 1 && height <= 200 else { return 0 }
-    guard array.filter({ $0 >= 1 && $0 <= 200}).count == array.count else { return 0 }
-    return array.filter({ $0 > height }).count
+    var count = 0
+    
+    for num in array {
+        if num > height { count += 1 }
+    }
+    
+    return count
 }
