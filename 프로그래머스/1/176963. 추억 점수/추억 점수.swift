a5ec8ffde1df45793f0f5339e5889a151bacf5d0
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-16 21:30:13 +0900

    [level 1] Title: 추억 점수, Time: 1.37 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.swift
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.swift
@@ -1,12 +1,23 @@
 import Foundation
 
 func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
-    let dict = name.enumerated()
-        .reduce(into: [String : Int]()) { dict, object in
-        dict[object.element, default: 0] += yearning[object.offset]
-        }
+    var result = [Int]()
+    var scoreDict = [String : Int]()
     
-    return photo.map {
-        $0.reduce(0) { $0 + (dict[$1] ?? 0) }
+    for (person, score) in zip(name, yearning) {
+        scoreDict[person] = score
     }
+    
+    for i in 0..<photo.count {
+        var tempScore = 0
+        
+        for j in 0..<photo[i].count {
+            let person = photo[i][j]
+            tempScore += scoreDict[person] ?? 0
+        }
+        
+        result.append(tempScore)
+    }
+    
+    return result
 }
