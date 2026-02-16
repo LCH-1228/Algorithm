e90a70d7a977199e4f4fb7baeb2ef6c930501569
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-16 22:33:37 +0900

    [level 1] Title: K번째수, Time: 0.16 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,18 +1,16 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    guard array.count >= 1 && array.count <= 100 else { return [] }
-    guard array.filter({$0 >= 1 && $0 <= 100}).count == array.count else { return [] }
-    guard commands.count >= 1 && commands.count <= 50 else { return [] }
-    guard commands.filter({ $0.count == 3 }).count == commands.count else { return [] }
     var result = [Int]()
-    var tempArray = [Int]()
+    
     for command in commands {
-        let i = command[0]
-        let j = command[1]
-        let k = command[2]
+        let i = command[0] - 1
+        let j = command[1] - 1
+        let k = command[2] - 1
         
-        result.append(array[i - 1...j - 1].sorted()[k - 1])
+        let num = Array(array[i...j]).sorted()
+        result.append(num[k])
     }
+    
     return result
 }
