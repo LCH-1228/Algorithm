cb959e3f16e10c450d23557eb43df26c717612e5
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-16 22:15:12 +0900

    [level 2] Title: 올바른 괄호, Time: 5.69 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -1,39 +1,17 @@
 import Foundation
 
 func solution(_ s:String) -> Bool {
-    guard s.first != ")" else { return false }
-    guard s.count <= 100000 else { return false }
-    let stack = Stack<Any>()
+    var count = 0
     
-    for character in s {
-        if character == "(" {
-            stack.push(character)
-        } else {
-            guard !stack.isEmpty else { return false }
-            stack.pop()
+    for char in s {
+        if char == "(" {
+            count += 1
+        } else if char == ")" {
+            count -= 1
         }
-    }
-    if stack.isEmpty {
-        return true
-    } else {
-        return false
-    }
-}
-
-//Stack 구현
-class Stack<T> {
-    
-    private var shared = [T]()
-    
-    var isEmpty: Bool {
-        return shared.count == 0 ? true : false
+        
+        if count < 0 { return false }
     }
     
-    func push(_ data: T) {
-        shared.append(data)
-    }
-    
-    func pop() {
-        shared.popLast()
-    }
+    return count == 0
 }
