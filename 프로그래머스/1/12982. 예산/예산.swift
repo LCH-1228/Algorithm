f4b9f37a012b00789a9697945eae1f7682f61815
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-23 09:12:09 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.swift
+++ 프로그래머스/1/12982. 예산/예산.swift
@@ -4,9 +4,9 @@ func solution(_ d:[Int], _ budget:Int) -> Int {
     var remain = budget
     var count = 0
     
-    for cost in d.sorted() {
-        if remain < cost { break }
-        remain -= cost
+    for price in d.sorted() {
+        if remain < price { break }
+        remain -= price
         count += 1
     }
     
