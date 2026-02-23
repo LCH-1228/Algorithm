6f328e5a93dbbf45e932d80806fe2a61f5266e54
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-24 07:02:55 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -2,16 +2,16 @@ import Foundation
 
 func solution(_ number:[Int]) -> Int {
     var count = 0
-    let n = number.indices
     
-    for i in n {
-        for j in n where j > i {
-            for k in n where k > j {
+    for i in 0..<number.count - 2 {
+        for j in (i + 1)..<number.count - 1 {
+            for k in (j + 1)..<number.count {
                 if number[i] + number[j] + number[k] == 0 {
                     count += 1
                 }
             }
         }
     }
+    
     return count
 }
