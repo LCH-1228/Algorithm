30a8ce9952d8cf19096ec0b5434c935c74d8b8ec
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-26 09:07:32 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
-    var index1 = 0
-    var index2 = 0
+    var card1Index = 0
+    var card2Index = 0
     
-    for word in goal {
-        if index1 < cards1.count && cards1[index1] == word {
-            index1 += 1
-        } else if index2 < cards2.count && cards2[index2] == word {
-            index2 += 1
+    for word in goal {   
+        if card1Index < cards1.count, cards1[card1Index] == word {
+            card1Index += 1
+        } else if card2Index < cards2.count, cards2[card2Index] == word {
+            card2Index += 1
         } else {
             return "No"
         }
     }
-    
+        
     return "Yes"
 }
