13668f28686a73d809d9ec23904a750f36c0da9f
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-02-28 13:43:08 +0900

    [level 1] Title: 달리기 경주, Time: 837.50 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.swift
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.swift
@@ -1,23 +1,24 @@
 import Foundation
 
 func solution(_ players:[String], _ callings:[String]) -> [String] {
-    var ranking = players
-    var playerDict = [String : Int]()
+    var result = players
+    var rankDict = [String: Int]()
     
-    for i in 0..<ranking.count {
-        playerDict[ranking[i]] = i
+    for (index, name) in players.enumerated() {
+        rankDict[name] = index
     }
     
-    for calling in callings {
-        let index = playerDict[calling]!
-        let frontName = ranking[index - 1]
+    for call in callings {
+        guard let back = rankDict[call] else { continue }
         
-        ranking[index - 1] = calling
-        ranking[index] = frontName
+        let front = back - 1
+        let frontName = result[front]
         
-        playerDict[calling] = index - 1
-        playerDict[frontName] = index
+        result.swapAt(front, back)
+        
+        rankDict[call] = front
+        rankDict[frontName] = back
     }
     
-    return ranking
+    return result
 }
