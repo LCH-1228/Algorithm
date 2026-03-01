596c3af2daa9de4aef0f27932e43fe304aa2e01b
LCH-1228 <107362287+LCH-1228@users.noreply.github.com>
2026-03-01 16:43:42 +0900

    [level 1] Title: 과일 장수, Time: 104.92 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -1,14 +1,24 @@
 import Foundation
 
 func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
-    var totalPrice = 0
-    let sortedScore = score.sorted(by: >)
-    let boxCount = sortedScore.count / m
+    var countList = Array(repeating: 0, count: k + 1)
     
-    for i in 0..<boxCount {
-        let minScore = sortedScore[i * m + (m - 1)]
-        totalPrice += minScore * m
+    for s in score {
+        countList[s] += 1
     }
     
-    return totalPrice
+    var result = 0
+    var leftover = 0
+    
+    for score in stride(from: k, through: 1, by: -1) {
+        let appleCount = countList[score] + leftover
+        
+        let boxCount = appleCount / m
+        
+        result += boxCount * score * m
+        
+        leftover = appleCount % m
+    }
+    
+    return result
 }
